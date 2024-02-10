841bf6f6b0797c85695f9f92e3dd65098db18a54
Wahyu Kevin <123295926+wahyukevin@users.noreply.github.com>
2024-02-10 07:17:17 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,3 @@
-const express = require('express');
-const server = express();
-
-server.all('/', (req, res) => {
-   res.write("I'm alive");
-   res.end();
-});
-
-function keepAlive() {
-   server.listen(80, () => {
-      console.log("Server is online!");
-   });
-}
-
-module.exports = keepAlive;
-
 const
   { Client } = require('discord.js-selfbot-v13'),
   dotenv = require('dotenv'),
